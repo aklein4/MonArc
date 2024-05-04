6713b247a5d09722b0a34873fef65a0fb3ed5b97
Adam Klein <adam3207@gmail.com>
2024-05-04 16:40:17 -0700

    batching

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -15,12 +15,12 @@ TOKENIZER_URL = "openai-community/gpt2"
 DATA_URL = 'HuggingFaceFW/fineweb'
 DATA_SUBSET = "CC-MAIN-2024-10"
 
-SAVE_PATH = "/home/aklein4/data/fw-50b"
-SAVE_REPO = 'fw-50b'
+SAVE_PATH = "/home/aklein4/data/fw-test"
+SAVE_REPO = 'fw-test'
 
-TRAIN_SIZE = # 5e10
-VAL_SIZE = # 1e9
-TEST_SIZE = # 1e9
+TRAIN_SIZE = 1e7 # 5e10
+VAL_SIZE = 1e7 # 1e9
+TEST_SIZE = 1e7 # 1e9
 
 MAX_LENGTH = 1024
 

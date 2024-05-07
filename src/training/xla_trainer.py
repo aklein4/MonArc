ad135de2f78a181468724413342b3f8072d37d02
Adam Klein <adam3207@gmail.com>
2024-05-07 03:23:29 -0700

    memory

--- src/training/xla_trainer.py
+++ src/training/xla_trainer.py
@@ -60,11 +60,7 @@ class XLATrainer:
                 loss = self._loss(logits, x)
 
             loss.backward()
-            xm.optimizer_step(optimizer, barrier=True)
+            xm.optimizer_step(optimizer, barrier=False)
 
             tracker.add(self.bs)
-            xm.add_step_closure(
-                lambda v: print(v.item()),
-                args=(loss,),
-            )
             

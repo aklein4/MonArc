7655efed041910b4d48198de1e46ffe9e61f45d2
Adam Klein <adam3207@gmail.com>
2024-05-07 01:30:40 -0700

    env fix

--- src/train_xla.py
+++ src/train_xla.py
@@ -6,8 +6,8 @@ import os
 from transformers import AutoTokenizer
 
 # from loaders.wds_loader import get_wds_loader
-from annelid.configuration_annelid import AnnelidConfig 
-from annelid.modeling_annelid import AnnelidLMModel
+# from annelid.configuration_annelid import AnnelidConfig 
+# from annelid.modeling_annelid import AnnelidLMModel
 # from training.xla_trainer import XLATrainer
 
 import utils.constants as constants

81be2a33edb171d72f5678dd5eb688d34a3fffe3
Adam Klein <adam3207@gmail.com>
2024-05-07 01:51:33 -0700

    error

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,8 +2,11 @@ import torch
 import torch_xla.core.xla_model as xm
 
 # # best device
-# DEVICE = "xla" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# XLA_DEVICE = xm.xla_device()
+DEVICE = "xla" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+XLA_DEVICE = None # xm.xla_device()
+def _init_xla():
+    global XLA_DEVICE
+    XLA_DEVICE = xm.xla_device()
 
 # # local data path
 # LOCAL_DATA_PATH = "./local_data"

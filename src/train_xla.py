de3703a2c14f3247a59e7cc241971b2d3410f53a
Adam Klein <adam3207@gmail.com>
2024-05-07 01:45:46 -0700

    error

--- src/train_xla.py
+++ src/train_xla.py
@@ -5,10 +5,10 @@ import os
 
 from transformers import AutoTokenizer
 
-from loaders.wds_loader import get_wds_loader
+# from loaders.wds_loader import get_wds_loader
 from annelid.configuration_annelid import AnnelidConfig 
 from annelid.modeling_annelid import AnnelidLMModel
-from training.xla_trainer import XLATrainer
+# from training.xla_trainer import XLATrainer
 
 import utils.constants as constants
 

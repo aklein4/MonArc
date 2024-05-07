ea026701e4bbf9f1849e455bab3acba0c2d586f9
Adam Klein <adam3207@gmail.com>
2024-05-07 01:48:13 -0700

    error

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,13 +1,13 @@
 import torch
 import torch_xla.core.xla_model as xm
 
-# best device
-DEVICE = "xla" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
-XLA_DEVICE = xm.xla_device()
+# # best device
+# DEVICE = "xla" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# XLA_DEVICE = xm.xla_device()
 
-# local data path
-LOCAL_DATA_PATH = "./local_data"
+# # local data path
+# LOCAL_DATA_PATH = "./local_data"
 
-# huggingface login id
-HF_ID = "aklein4"
-HF_TOKEN = None
+# # huggingface login id
+# HF_ID = "aklein4"
+# HF_TOKEN = None

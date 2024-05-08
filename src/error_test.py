4a9c5f03a4c8ce534defdaf4e62fc911ffba6092
Adam Klein <adam3207@gmail.com>
2024-05-08 00:31:29 -0700

    simpler

--- src/error_test.py
+++ src/error_test.py
@@ -9,9 +9,6 @@ from loaders.wds_loader import get_wds_loader
 from transformers import AutoTokenizer
 import utils.constants as constants
 
-from annelid.configuration_annelid import AnnelidConfig 
-from annelid.modeling_annelid import AnnelidLMModel
-
 import os
 
 
@@ -26,46 +23,12 @@ class TestModel(nn.Module):
 
 def _mp_fn(index):
     torch.set_default_dtype(torch.float32)
-    constants._init_xla()
-    
-    MODEL_CONFIG = {
-        "model_type": "annelid",
-        "architectures": [
-            "AnnelidLMModel"
-        ],
-
-        "bos_token_id": 50256,
-        "eos_token_id": 50256,
-        "hidden_act": "silu",
-        "hidden_size": 128,
-        "initializer_range": 0.02,
-        "intermediate_size": 128*3,
-        "max_position_embeddings": 1024,
-        "layer_norm_eps": 1e-05,
-        "num_attention_heads": 2,
-        "num_hidden_layers": 2,
-        "num_key_value_heads": 2,
-        "partial_rotary_factor": 0.25,
-        "rope_theta": 10000,
-        "tie_word_embeddings": False,
-
-        "vocab_size": 60000, # 50258, # with padding token
-
-        "is_prefix_lm": False,
-        "is_quasi_lm": False,
-        "segment_size": 32,
-        "use_segment_embeds": False,
-
-        "_attn_implementation": "sdpa",
-    }
 
     tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
     loader = get_wds_loader("fw-4b", "train", tokenizer, 1024, True, 1)
 
-    config = AnnelidConfig(**MODEL_CONFIG)
-    # model = AnnelidLMModel(config).to(constants.XLA_DEVICE)
     model = TestModel().to(constants.XLA_DEVICE)
 
     for p in model.parameters():

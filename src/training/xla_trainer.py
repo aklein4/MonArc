4c84e570ef61467a45b12ae371d6b96a297aaa36
Adam Klein <adam3207@gmail.com>
2024-05-07 20:49:52 -0700

    restore

--- src/training/xla_trainer.py
+++ src/training/xla_trainer.py
@@ -50,8 +50,8 @@ class XLATrainer:
         optimizer = syncfree.AdamW(self.model.parameters(), lr=self.lr)
 
         tracker = xm.RateTracker()
-        for x in self.loader:
-            x = x.to(constants.XLA_DEVICE)
+        for _ in range(100000):
+            x = torch.zeros(self.bs, 1024).long().to(constants.XLA_DEVICE)
 
             optimizer.zero_grad()
 

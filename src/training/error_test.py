a74abf702b033a08c1dc929c07eb8b99edea79ce
Adam Klein <adam3207@gmail.com>
2024-05-07 20:05:56 -0700

    error test

--- src/training/error_test.py
+++ src/training/error_test.py
@@ -4,15 +4,6 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 
 import os
 
-from transformers import AutoTokenizer
-
-from loaders.wds_loader import get_wds_loader
-from annelid.configuration_annelid import AnnelidConfig 
-from annelid.modeling_annelid import AnnelidLMModel
-from training.xla_trainer import XLATrainer
-
-import utils.constants as constants
-
 
 def _mp_fn(index):
     x = torch.zeros(1, 2, 3, device=xm.xla_device())

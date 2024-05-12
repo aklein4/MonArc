f870910640e2cc2e2d8ea33b1c5aeb8d405a0f00
Adam Klein <adam3207@gmail.com>
2024-05-12 00:22:18 -0700

    pcorr fix

--- src/trainers/xla_arc_trainer.py
+++ src/trainers/xla_arc_trainer.py
@@ -18,7 +18,7 @@ class XLAArcTrainer(BaseXLATrainer):
             lm_loss=loss(out.lm_logits, x, ignore_index),
             lm_ppl=ppl(out.lm_logits, x, ignore_index),
             lm_acc=acc(out.lm_logits, x, ignore_index),
-            pcorr=pcorr(out.lm_logits, x, ignore_index),
+            lm_pcorr=pcorr(out.lm_logits, x, ignore_index),
 
             arc_loss=loss(out.arc_preds, out.arc_targets, -1),
             arc_ppl=ppl(out.arc_preds, out.arc_targets, -1),

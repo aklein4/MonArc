2126d4734091502a810f571c71a0c8b939435398
Adam Klein <adam3207@gmail.com>
2024-05-13 22:15:54 -0700

    arc fix

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,19 +3,20 @@
 from models.annelid.modeling_annelid import AnnelidLMModel
 from models.annelid.configuration_annelid import AnnelidConfig
 
-from models.arc import ArcLmModel
+from models.arc_old.modeling_arc import ArcLMModel
+from models.arc_old.configuration_arc import ArcConfig
 
 from models.base import BaseConfig, BaseLmModel
 
 
 CONFIG_DICT = {
     "annelid": AnnelidConfig,
-    "arc": BaseConfig,
+    "arc": ArcConfig,
     "base": BaseConfig,
 }
 
 MODEL_DICT = {
     "annelid": AnnelidLMModel,
-    "arc": ArcLmModel,
+    "arc": ArcLMModel,
     "base": BaseLmModel,
 }

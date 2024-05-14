906623982808a26c7da0c656066770d27a5e1705
Adam Klein <adam3207@gmail.com>
2024-05-13 19:08:01 -0700

    base

--- src/test_base.py
+++ src/test_base.py
@@ -2,8 +2,6 @@ import torch
 
 from transformers import AutoTokenizer
 
-from models.annelid.modeling_annelid import AnnelidLMModel
-from models.annelid.configuration_annelid import AnnelidConfig
 from models.base import BaseConfig, BaseLmModel
 from utils.config_utils import load_model_config
 import utils.constants as constants
@@ -13,42 +11,34 @@ MODEL_CONFIG = 'test-lm'
 
 
 def main():
-    
+
+
     print("Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(constants.GPT2_TOKENIZER, resume_download=None)
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
     
     x = tokenizer(["Hello, my dog is cute", "His dog is cute too", "All dogs are cute"], return_tensors="pt", padding="max_length", max_length=16).input_ids
 
-    print("loading base...")
-    base_config = BaseConfig(**load_model_config(MODEL_CONFIG, tokenizer))
-    base_model = BaseLmModel(base_config)
+    print("loading config...")
+    config = load_model_config(MODEL_CONFIG, tokenizer)
 
-    print("loading annelid...")
-    annelid_model_config = load_model_config(MODEL_CONFIG, tokenizer)
-    annelid_model_config["segment_size"] = 4
+    print("loading eager...")
+    eager_model = BaseLmModel(BaseConfig(**config)).to(constants.XLA_DEVICE())
 
-    annelid_config = AnnelidConfig(**annelid_model_config)
-    print(annelid_config.use_parallel_residual)
-    annelid_model = AnnelidLMModel(annelid_config)
+    print("loading flash attention...")
+    config["_attn_implementation"] = 'flash_attention_2_xla'
+    flash_model = BaseLmModel(BaseConfig(**config)).to(constants.XLA_DEVICE())
 
     print("copying weights...")
-    base_model.load_state_dict(annelid_model.state_dict(), strict=True)
-    
-    base_model.eval()
-    annelid_model.eval()
+    flash_model.load_state_dict(eager_model.state_dict(), strict=True)
 
-    annelid_out = annelid_model(x)
-    base_out = base_model(x)
+    eager_out = eager_model(x)
+    flash_out = flash_model(x)
 
-    diff = torch.abs(annelid_out.lm_logits - base_out.lm_logits).max()
-    print(f"Diff: {diff}")
+    diff = torch.abs(eager_out.lm_logits - flash_out.lm_logits).max()
+    print(f"Diff: {diff.item()}", flush=True)
 
 
 if __name__ == '__main__':
-    torch.set_printoptions(threshold=10_000)
-
-    import os
-    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-
     main()
+    

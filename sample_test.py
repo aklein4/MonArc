28ca3cb5912b51799305db3e5f60a0ba1a6dbbfd
Adam Klein <adam3207@gmail.com>
2024-05-16 03:28:38 -0700

    hierarchical sampling

--- sample_test.py
+++ sample_test.py
@@ -4,27 +4,38 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-D = 10
+
 SAMPLES = 100000
 
 
 def main():
     
-    logits = torch.randn(D)
-    probs = torch.softmax(logits, dim=0)
-    cum_probs = torch.cumsum(probs, dim=0)
-    print(cum_probs)
+        lm_logits = torch.randn(15)
+        vocab_factor = 5
+        vocab_chunk = 3
 
-    counts = torch.zeros(D)
-    for i in tqdm(range(SAMPLES)):
-        r = torch.rand(1)
-        counts[(r > cum_probs).long().sum()] += 1
-    counts /= SAMPLES 
+        factored_probs = torch.softmax(
+            lm_logits.detach().float(), dim=-1
+        ).view(vocab_factor, vocab_chunk)
 
-    plt.bar(range(D), counts.numpy(), alpha=0.5, label="sampled")
-    plt.bar(range(D), probs.numpy(), alpha=0.5, label="true")
-    plt.legend()
-    plt.show()
+        counts = torch.zeros(15)
+        for i in tqdm(range(1000)):
+
+            outer_probs = factored_probs.sum(dim=-1)
+            outer_sample = torch.multinomial(outer_probs, 1, True)[:, 0]
+
+            inner_probs = factored_probs[outer_sample]
+            inner_sample = torch.multinomial(inner_probs, 1, True)[:, 0]
+
+            sample = vocab_chunk*outer_sample + inner_sample
+            counts[sample] += 1
+
+        counts /= counts.sum()
+
+        plt.bar(range(15), counts, alpha=0.5, label='sampled')
+        plt.bar(range(15), factored_probs.view(-1), alpha=0.5, label='true')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':

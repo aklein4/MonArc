57318cd527e7f5ed7a4e8d154055aa65520583e8
Adam Klein <adam3207@gmail.com>
2024-05-16 04:21:41 -0700

    compile med

--- src/train_xla.py
+++ src/train_xla.py
@@ -50,7 +50,7 @@ def _mp_fn(index, args):
         model = model.to(torch.float32)
     
     # log_print("Compiling model...")
-    # model.model = torch.compile(model.model, backend='openxla')
+    model.model = torch.compile(model.model, backend='openxla')
 
     log_print("Loading data...")
     loader = get_wds_loader(

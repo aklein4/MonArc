6857b3b79e5323d2005fccff7fe6d20b51ed6aee
Adam Klein <adam3207@gmail.com>
2024-05-17 03:04:57 -0700

    arc acc eps

--- src/models/monarc.py
+++ src/models/monarc.py
@@ -238,9 +238,7 @@ class MonArcHeadTransformer(BaseTransformer):
 
         # get inputs
         if input_ids is not None:
-            extra = self._get_tokens(input_ids)
-            log_print(extra)
-            hidden_states = hidden_states + extra
+            hidden_states = hidden_states + self._get_tokens(input_ids)
         else:
             input_ids = torch.zeros(hidden_states.shape[:-1], dtype=torch.long, device=hidden_states.device)
         
@@ -398,7 +396,7 @@ class MonArcLmModel(BaseModel):
             attention_mask=torch.cat([attention_mask, attention_mask], dim=0),
             cached_mask=True
         )[0].chunk(2, dim=0)
-
+        
         true_logits = torch.bmm(
             self.lm_head.weight[true_labels.view(-1)].unsqueeze(-2),
             true_states.view(-1, true_states.shape[-1]).unsqueeze(-1)

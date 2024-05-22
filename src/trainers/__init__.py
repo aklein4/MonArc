696ff5fafaa642d18e27f7cec0437a992c0ba09a
Adam Klein <adam3207@gmail.com>
2024-05-21 19:01:56 -0700

    deprecated monarc

--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -2,11 +2,9 @@
 
 from trainers.xla_lm_trainer import XLALMTrainer
 from trainers.xla_arc_trainer import XLAArcTrainer
-from trainers.xla_monarc_trainer import XLAMonArcTrainer
 
 
 TRAINER_DICT = {
     "XLALMTrainer": XLALMTrainer,
     "XLAArcTrainer": XLAArcTrainer,
-    "XLAMonArcTrainer": XLAMonArcTrainer
 }

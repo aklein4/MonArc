790607a49ab58b1542d985d3187b524a6b11d9c0
Adam Klein <adam3207@gmail.com>
2024-05-21 20:07:29 -0700

    removed batch divisor and arc_adj

--- src/trainers/xla_arc_trainer.py
+++ src/trainers/xla_arc_trainer.py
@@ -6,19 +6,17 @@ from trainers.base_xla_trainer import BaseXLATrainer
 from utils.data_utils import DotDict
 from  utils.training_utils import (
     loss, ppl, acc, pcorr,
-    arc_loss, arc_acc, arc_pcorr
+    arc_loss, arc_acc, arc_pcorr,
+    arc_adj
 )
 
 
 class XLAArcTrainer(BaseXLATrainer):
 
     def train_step(self, model, x, seg_ids, tokenizer):
-        lm_logits, true_arc, fake_arc = model.forward(x, segment_ids=seg_ids)
         ignore_index = tokenizer.pad_token_id
-        try:
-            divisor = model.arc_divisor
-        except AttributeError:
-            divisor = 1
+        
+        lm_logits, true_arc, fake_arc = model.forward(x, segment_ids=seg_ids)
 
         results = DotDict(
             lm_loss=loss(lm_logits, x, ignore_index),
@@ -26,10 +24,14 @@ class XLAArcTrainer(BaseXLATrainer):
             lm_acc=acc(lm_logits, x, ignore_index),
             lm_pcorr=pcorr(lm_logits, x, ignore_index),
 
-            arc_loss=arc_loss(true_arc, fake_arc, x, ignore_index, divisor),
-            arc_acc=arc_acc(true_arc, fake_arc, x, ignore_index, divisor),
-            arc_pcorr=arc_pcorr(true_arc, fake_arc, x, ignore_index, divisor)
+            arc_loss=arc_loss(true_arc, fake_arc, x, ignore_index),
+            arc_acc=arc_acc(true_arc, fake_arc, x, ignore_index),
+            arc_pcorr=arc_pcorr(true_arc, fake_arc, x, ignore_index),
+            arc_adj=arc_adj(true_arc, fake_arc, x, ignore_index)
         )
         results.loss = results.lm_loss + self.w_arc * results.arc_loss
+
+        # a little extra, as a treat
+        results.lm_loss_adj = results.lm_loss.detach() - results.arc_adj
 
         return results
--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -59,70 +59,72 @@ def pcorr(logits, x, ignore_index):
     return p.sum() / (mask).float().sum()
 
 
-def arc_loss(true_arc, fake_arc, input_ids, ignore_index, divisor=1):
-    assert input_ids.shape[0] % divisor == 0
-    div_bs = input_ids.shape[0] // divisor
-
+def arc_loss(true_arc, fake_arc, input_ids, ignore_index):
     true_arc = true_arc[:, :-1].view(-1)
     fake_arc = fake_arc[:, :-1].view(-1)
-    true_input_ids = input_ids[:, 1:].view(-1)
-    fake_input_ids = input_ids[:div_bs, 1:].view(-1)
+    input_ids = input_ids[:, 1:].view(-1)
 
     true_loss = F.logsigmoid(-true_arc)
     fake_loss = F.logsigmoid(fake_arc)
 
-    true_mask = true_input_ids != ignore_index
-    fake_mask = fake_input_ids != ignore_index
-    true_loss = torch.masked_fill(true_loss, ~true_mask, 0.0)
-    fake_loss = torch.masked_fill(fake_loss, ~fake_mask, 0.0)
+    mask = input_ids != ignore_index
+    true_loss = torch.masked_fill(true_loss, ~mask, 0.0)
+    fake_loss = torch.masked_fill(fake_loss, ~mask, 0.0)
 
     return -(
-        true_loss.sum()/true_mask.float().sum() +
-        fake_loss.sum()/fake_mask.float().sum()
-    )
+        true_loss.sum() +
+        fake_loss.sum()
+    )/mask.float().sum()
 
 
-def arc_acc(true_arc, fake_arc, input_ids, ignore_index, divisor=1):
-    assert input_ids.shape[0] % divisor == 0
-    div_bs = input_ids.shape[0] // divisor
-    
+@torch.no_grad()
+def arc_acc(true_arc, fake_arc, input_ids, ignore_index):
     true_arc = true_arc[:, :-1].view(-1)
     fake_arc = fake_arc[:, :-1].view(-1)
-    true_input_ids = input_ids[:, 1:].view(-1)
-    fake_input_ids = input_ids[:div_bs, 1:].view(-1)
+    input_ids = input_ids[:, 1:].view(-1)
     
     true_acc = (true_arc < 0).float()
     fake_acc = (fake_arc >= 0).float()
 
-    true_mask = true_input_ids != ignore_index
-    fake_mask = fake_input_ids != ignore_index
-    true_acc = torch.masked_fill(true_acc, ~true_mask, 0.0)
-    fake_acc = torch.masked_fill(fake_acc, ~fake_mask, 0.0)
+    mask = input_ids != ignore_index
+    true_acc = torch.masked_fill(true_acc, ~mask, 0.0)
+    fake_acc = torch.masked_fill(fake_acc, ~mask, 0.0)
 
     return 0.5 * (
-        true_acc.sum()/true_mask.float().sum() +
-        fake_acc.sum()/fake_mask.float().sum()
-    )
+        true_acc.sum() +
+        fake_acc.sum()
+    )/mask.float().sum()
 
 
-def arc_pcorr(true_arc, fake_arc, input_ids, ignore_index, divisor=1):
-    assert input_ids.shape[0] % divisor == 0
-    div_bs = input_ids.shape[0] // divisor
-
+@torch.no_grad()
+def arc_pcorr(true_arc, fake_arc, input_ids, ignore_index):
     true_arc = true_arc[:, :-1].view(-1)
     fake_arc = fake_arc[:, :-1].view(-1)
-    true_input_ids = input_ids[:, 1:].view(-1)
-    fake_input_ids = input_ids[:div_bs, 1:].view(-1)
+    input_ids = input_ids[:, 1:].view(-1)
 
     true_p = torch.sigmoid(-true_arc)
     fake_p = torch.sigmoid(fake_arc)
 
-    true_mask = true_input_ids != ignore_index
-    fake_mask = fake_input_ids != ignore_index
-    true_p = torch.masked_fill(true_p, ~true_mask, 0.0)
-    fake_p = torch.masked_fill(fake_p, ~fake_mask, 0.0)
+    mask = input_ids != ignore_index
+    true_p = torch.masked_fill(true_p, ~mask, 0.0)
+    fake_p = torch.masked_fill(fake_p, ~mask, 0.0)
 
     return 0.5 * (
-        true_p.sum()/true_mask.float().sum() +
-        fake_p.sum()/fake_mask.float().sum()
-    )
+        true_p.sum() + 
+        fake_p.sum()
+    )/mask.float().sum()
+
+
+@torch.no_grad()
+def arc_adj(true_arc, fake_arc, input_ids, ignore_index):
+    true_arc = true_arc[:, :-1].view(-1)
+    fake_arc = fake_arc[:, :-1].view(-1)
+    input_ids = input_ids[:, 1:].view(-1)
+
+    # appriximate log prob gain when z=0
+    adj = -true_arc
+
+    mask = input_ids != ignore_index
+    adj = torch.masked_fill(adj, ~mask, 0.0)
+
+    return adj/mask.float().sum()

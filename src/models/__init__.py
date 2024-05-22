a281e05f8e54cea08b9ec3d9cd31700c6f4b74ce
Adam Klein <adam3207@gmail.com>
2024-05-21 19:58:47 -0700

    deprecated embarc

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,16 +2,13 @@
 
 from models.base import BaseConfig, BaseLmModel
 from models.arc import ArcConfig, ArcLmModel
-from models.embarc import EmbArcLmModel
 
 CONFIG_DICT = {
     "base": BaseConfig,
     "arc": ArcConfig,
-    "embarc": BaseConfig,
 }
 
 MODEL_DICT = {
     "base": BaseLmModel,
     "arc": ArcLmModel,
-    "embarc": EmbArcLmModel,
 }

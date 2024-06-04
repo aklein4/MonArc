a23fcdb6de78457fe9989cd51caaf5bd793f80d4
Adam Klein <adam3207@gmail.com>
2024-06-03 23:20:31 -0700

    test

--- src/trainers/base_xla_trainer.py
+++ src/trainers/base_xla_trainer.py
@@ -156,6 +156,7 @@ class BaseXLATrainer:
         curr_step = 0
         token_tracker = xm.RateTracker()
         step_tracker = xm.RateTracker()
+        log_master_print("before loop")
         for x, seg_ids in loader:
             assert x.shape == seg_ids.shape, f"Input ({x.shape}) and segment ids ({seg_ids.shape}) must have same shape!"
 
@@ -189,10 +190,12 @@ class BaseXLATrainer:
                                 results_accum[k] = 0.0
                             results_accum[k] = results_accum[k] + v.detach()
                 
-                # mark step to save gradients
+                log_master_print("before backward")
                 results.loss.backward()
+                log_master_print("after backward")
                 if len(x_split) > 1:
                     xm.mark_step()
+                log_master_print("after mark step")
 
             # perform a single optimizer step
             xm.optimizer_step(optimizer)
